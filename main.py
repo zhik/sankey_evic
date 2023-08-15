225cdfa2b14f4d9299b1f7710b353aca8995ede6
Zhi Keng He <zhikhe@gmail.com>
2023-08-15 12:22:13 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import sqlalchemy as sa
+from sqlalchemy import text
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+ENGINE = sa.create_engine(os.environ["DATABASE_URL"])
+
+query = f"""
+    select * from (
+	select indexnumberid, fileddate, status, disposedreason from oca_index
+	where fileddate >= '2023-07-04'
+	and 
+	propertytype = 'Residential'
+	and 
+	(classification in ('Holdover', 'Non-Payment'))
+	and
+	status != 'Active'
+	and
+	disposedreason not LIKE 'Withdrawn%' ) as a 
+	left join (select * from oca_appearances where appearancereason != 'New Appearance') as b on  a.indexnumberid = b.indexnumberid
+"""
+df = pd.read_sql(text(query), con = ENGINE)
+
+print(df)
